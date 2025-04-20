73428f7bbb770a8cfd22dcaa5f3f228189273173
izzy <izzy@urchinator.com>
2025-04-19 18:00:04 -0600

    quad/point intersection

--- intersect/triangle.c
+++ intersect/triangle.c
@@ -1,5 +1,42 @@
 
 
+Vector3 baryCoords2(Vector2 p, Vector2 a, Vector2 b, Vector2 c) {
+
+	vec2 ab = vSub(b, a);
+	vec2 ac = vSub(c, a);
+	vec2 ap = vSub(p, a);
+	float invden = 1.f / (ab.x * ac.y - ac.x * ab.y);
+	
+	Vector3 out;
+	
+	out.y = (ap.x * ac.y - ac.x * ap.y) * invden;
+	out.z = (ab.x * ap.y - ap.x * ab.y) * invden;
+	out.x = 1.0f - out.y - out.z;
+	
+	return out;
+}
+
+
+// returns the *signed* area of a triangle. useful for determining winding
+// positive values mean a clockwise triangle
+float triArea2p(Vector2* a, Vector2* b, Vector2* c) {
+	return 0.5 * (
+		((b->x - a->x) * (b->y + a->y)) +
+		((c->x - b->x) * (c->y + b->y)) +
+		((a->x - c->x) * (a->y + c->y)));
+}
+
+
+// determines if a point is inside a triangle
+int triPointInside2p(Vector2* p, Vector2* a, Vector2* b, Vector2* c) {
+	int d = signbit((p->x - b->x) * (a->y - b->y) - (a->x - b->x) * (p->y - b->y));
+	int e = signbit((p->x - c->x) * (b->y - c->y) - (b->x - c->x) * (p->y - c->y));
+	if(d != e) return 0;
+	int f = signbit((p->x - a->x) * (c->y - a->y) - (c->x - a->x) * (p->y - a->y));
+	return e == f;
+}
+
+
 
 
 float distLine2Triangle2(Line2 a, Vector2 tri[3]) {
@@ -23,6 +60,7 @@ float distLine2Triangle2(Line2 a, Vector2 tri[3]) {
 	return MIN(MIN(d[0], d[1]), d[2]);
 }
 
+
 float distPoint2Triangle2(Vector2 a, Vector2 tri[3]) {
 	
 	// check if the point is inside the triangle,
@@ -41,4 +79,5 @@ float distPoint2Triangle2(Vector2 a, Vector2 tri[3]) {
 	// the point is outside; one of the corners is closest, find which one
 	return MIN(MIN(d[0], d[1]), d[2]);
 }
+
 
